20453ed80192e11351e1139c998fac628285d56c
PimLeerkes <pimsimon.leerkes@gmail.com>
2024-07-02 16:56:22 +0200

    extended map from only sparsematrix to whole dtmc

--- stormvogel/map.py
+++ stormvogel/map.py
@@ -2,10 +2,45 @@ import stormpy.storage
 import stormvogel.model
 
 
-def simple_to_matrix(model: stormvogel.model.Model) -> stormpy.storage.SparseMatrix:
+def stormvogel_to_stormpy_dtmc(
+    model: stormvogel.model.Model,
+) -> stormpy.storage.SparseDtmc:
     """
-    Takes a simple representation as input and outputs a sparsematrix
+    Takes a simple representation as input and outputs a dtmc how it is represented in stormpy
     """
+
+    # we first build the SparseMatrix
+    builder = stormpy.storage.SparseMatrixBuilder()
+    for transition in model.transitions.items():
+        for branch in transition[1].transition.values():
+            for tuple in branch.branch:
+                builder.add_next_value(
+                    row=transition[0], column=tuple[1].id, value=tuple[0]
+                )
+    matrix = builder.build()
+
+    # then we add the labels
+    state_labeling = stormpy.storage.StateLabeling(len(model.states))
+    for state in model.states.items():
+        for label in state[1].labels:
+            state_labeling.add_label(label)
+            state_labeling.add_label_to_state(label, state[0])
+
+    # TODO rewards
+
+    # then we build the dtmc
+    components = stormpy.SparseModelComponents(
+        transition_matrix=matrix, state_labeling=state_labeling
+    )
+    dtmc = stormpy.storage.SparseDtmc(components)
+
+    return dtmc
+
+
+"""
+def stormvogel_to_stormpy_mdp(model: stormvogel.model.Model) -> stormpy.storage.SparseMdp:
+    Takes a simple representation as input and outputs an mdp how it is represented in stormpy
+
     builder = stormpy.storage.SparseMatrixBuilder()
 
     for transition in model.transitions.items():
@@ -17,10 +52,17 @@ def simple_to_matrix(model: stormvogel.model.Model) -> stormpy.storage.SparseMat
 
     matrix = builder.build()
     return matrix
+"""
+"""
+def stormpy_to_stormvogel_dtmc(matrix: stormpy.SparseMatrix) -> stormvogel.model.Model:
 
+    Takes a sparsematrix as input and outputs a simple representation
+
+    return simple
+"""
 
 """
-def matrix_to_simple(matrix: stormpy.SparseMatrix) -> stormvogel.model.Model:
+def stormpy_to_stormvogel_mdp(matrix: stormpy.SparseMatrix) -> stormvogel.model.Model:
 
     Takes a sparsematrix as input and outputs a simple representation
 
@@ -38,5 +80,4 @@ if __name__ == "__main__":
         [(1 / 6, dtmc.new_state(f"rolled{i}", {"rolled": i})) for i in range(6)]
     )
 
-    print(simple_to_matrix(dtmc))
-    "test"
+    print(stormvogel_to_stormpy_dtmc(dtmc))
