f21e9eadd33ff4788a94430a102eec85b8a4b576
PimLeerkes <pimsimon.leerkes@gmail.com>
2024-07-05 13:40:13 +0200

    minimal version of mapping from stormpy to stormvogel dtmc now working

--- stormvogel/map.py
+++ stormvogel/map.py
@@ -37,51 +37,53 @@ def stormvogel_to_stormpy_dtmc(
     return dtmc
 
 
-"""
-def stormpy_to_stormvogel_dtmc(matrix: stormpy.SparseMatrix) -> stormvogel.model.Model:
+def stormpy_to_stormvogel_dtmc(
+    sparsedtmc: stormpy.storage.SparseDtmc,
+) -> stormvogel.model.Model:
+    """
     Takes a dtmc stormpy representation as input and outputs a simple stormvogel representation
+    """
 
+    # we create the model
+    model = stormvogel.model.new_dtmc(name=None)
 
+    # we add the states and the transitions
+    matrix = sparsedtmc.transition_matrix
+    for state in sparsedtmc.states:
+        # the initial state is automatically added so we don't add it
+        if state.id > 0:
+            model.new_state(labels=list(state.labels))
 
-    return simple
-"""
+        # we add the transitions
+        row = matrix.get_row(state.id)
+        transitionshorthand = [(x.value(), x.column) for x in row]
+        transitions = stormvogel.model.transition_from_shorthand(transitionshorthand)
+        model.set_transitions(state, transitions)
+
+    # TODO rewards
+
+    return model
 
 
 """
 def stormvogel_to_stormpy_mdp(model: stormvogel.model.Model) -> stormpy.storage.SparseMdp:
     Takes a simple representation as input and outputs an mdp how it is represented in stormpy
 
-    builder = stormpy.storage.SparseMatrixBuilder()
-
-    for transition in model.transitions.items():
-        for branch in transition[1].transition.values():
-            for tuple in branch.branch:
-                builder.add_next_value(
-                    row=transition[0], column=tuple[1].id, value=tuple[0]
-                )
-
-    matrix = builder.build()
-    return matrix
 """
 
 
 """
 def stormpy_to_stormvogel_mdp(matrix: stormpy.SparseMatrix) -> stormvogel.model.Model:
-
-    Takes a sparsematrix as input and outputs a simple representation
-
-    return simple
+    Takes a stormpy representation of an mdp as input and outputs a stormvogel model representation
 """
 
+
 if __name__ == "__main__":
-    # Create a new model with the name "Die"
     dtmc = stormvogel.model.new_dtmc("Die")
-
     init = dtmc.get_initial_state()
-
-    # From the initial state, add the transition to 6 new states with probability 1/6th.
     init.set_transitions(
         [(1 / 6, dtmc.new_state(f"rolled{i}", {"rolled": i})) for i in range(6)]
     )
-
-    print(stormvogel_to_stormpy_dtmc(dtmc))
+    print(dtmc)
+    sparsedtmc = stormvogel_to_stormpy_dtmc(dtmc)
+    print(stormpy_to_stormvogel_dtmc(sparsedtmc))
