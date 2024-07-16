a3a99212ffba80796099a76e693ef38d115fc7ae
Linus <linus.heck@rwth-aachen.de>
2024-07-16 16:29:18 +0200

    Parse PRISM in notebook (#35)
    * Parse PRISM in notebook
    
    * Print model in notebook
    
    * Check whether we are in notebook in __init__
    
    ---------
    
    Co-authored-by: Linus Heck <linus.heck@ru.nl>

--- stormvogel/__init__.py
+++ stormvogel/__init__.py
@@ -1,1 +1,21 @@
 """The stormvogel package"""
+
+
+def is_in_notebook():
+    try:
+        from IPython.core.getipython import get_ipython
+
+        ipython = get_ipython()
+
+        if ipython is None or "IPKernelApp" not in ipython.config:
+            return False
+    except ImportError:
+        return False
+    except AttributeError:
+        return False
+    return True
+
+
+if is_in_notebook():
+    # Import and init magic
+    from stormvogel import magic as magic
