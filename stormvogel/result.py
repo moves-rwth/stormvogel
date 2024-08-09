c411612a9318399daac275e536f403fca2877c80
PimLeerkes <pimsimon.leerkes@gmail.com>
2024-08-09 14:28:28 +0200

    converting results now seems to work with schedulers

--- stormvogel/result.py
+++ stormvogel/result.py
@@ -8,41 +8,51 @@ class Result:
     """Result object represents the model checking results for a given model
 
     Args:
-        probabilities: for each state the model checking result
+        values: for each state the model checking result
         scheduler: in case the model is an mdp we can optionally store a scheduler
     """
 
-    probabilities: list[int]
-    scheduler: dict[int, stormvogel.model.Action] | None
+    values: list[stormvogel.model.Number]
 
-    def __init__(self, probabilities: list[int]):
-        self.probabilities = probabilities
+    # TODO link states to actual actions instead of only a string?
+    scheduler: dict[int, str] | None
+
+    def __init__(self, values: list[stormvogel.model.Number]):
+        self.values = values
         self.scheduler = None
 
-    def add_scheduler(self, scheduler: dict[int, stormvogel.model.Action] | None):
-        """adds a scheduler to the results"""
-        self.scheduler = scheduler
+    def add_scheduler(self, stormpy_scheduler: stormpy.storage.Scheduler):
+        """adds a scheduler to the result"""
+        stormvogel_scheduler = {}
+        for i in range(len(self.values)):
+            stormvogel_scheduler[i] = str(stormpy_scheduler.get_choice(i))
+        self.scheduler = stormvogel_scheduler
 
-    def get_result_of_state(self, state: stormvogel.model.State):
+    def get_result_of_state(
+        self, state: stormvogel.model.State
+    ) -> stormvogel.model.Number:
         """returns the model checking result for a given state"""
-        return self.probabilities[state.id]
+        return self.values[state.id]
+
+    def get_choice_of_state(self, state: stormvogel.model.State) -> str | None:
+        """returns the choice in the sceduler for the given state"""
+        if self.scheduler is not None:
+            return self.scheduler[state.id]
+        else:
+            print("this result does not have a scheduler")
+            return None
 
 
-def convert_model_checking_results(
+def convert_model_checking_result(
     stormpy_result: stormpy.core.ExplicitQuantitativeCheckResult,
+    with_scheduler: bool = True,
 ) -> Result:
     """
     Takes a model checking result from stormpy and converts it to a stormvogel representation
     """
-    # print(dir(result))
-
-    # print(result.get_values())
-
     stormvogel_result = Result(stormpy_result.get_values())
-    if stormpy_result.has_scheduler:
-        print("scheduler:", stormpy_result.scheduler)
-        scheduler = None
-        stormvogel_result.add_scheduler(scheduler)
+    if stormpy_result.has_scheduler and with_scheduler:
+        stormvogel_result.add_scheduler(stormpy_result.scheduler)
 
     return stormvogel_result
 
@@ -55,5 +65,5 @@ if __name__ == "__main__":
 
     model = stormpy.build_model(prism_program, properties)
     result = stormpy.model_checking(model, properties[0])
-    # print(type(result))
-    convert_model_checking_results(result)
+    print(type(result))
+    convert_model_checking_result(result)
