497c104d3e4fdc5bee77ef7277f1dcc7f9d98ee0
PimLeerkes <pimsimon.leerkes@gmail.com>
2024-08-20 19:25:21 +0200

    scheduler comparison now also works in test

--- tests/test_result.py
+++ tests/test_result.py
@@ -31,16 +31,22 @@ def test_convert_model_checker_results_mdp():
     model = stormpy.build_model(prism_program, properties)
     result = stormpy.model_checking(model, properties[0], extract_scheduler=True)
 
-    # print(dir(result.scheduler))
-
-    # print(result.scheduler.get_choice(0).get_choice())
-
     stormvogel_model = stormvogel.map.stormpy_to_stormvogel(model)
     if stormvogel_model is not None:
         stormvogel_result = stormvogel.result.convert_model_checking_result(
             stormvogel_model, result
         )
         assert list(stormvogel_result.values.values()) == result.get_values()
-        # assert [result.scheduler.get_choice(state.id).get_choice() for state in model.states] == list(stormvogel_result.scheduler.taken_actions.values())
+
+        if stormvogel_result.scheduler is not None:
+            assert [
+                result.scheduler.get_choice(state.id).get_deterministic_choice()
+                for state in model.states
+            ] == [
+                int(list(action.labels)[0])
+                for action in stormvogel_result.scheduler.taken_actions.values()
+            ]
+        else:
+            assert False
     else:
         assert False
