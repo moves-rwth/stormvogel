7c6aac531f8b168f710c3d79ea457b8819b41914
PimLeerkes <pimsimon.leerkes@gmail.com>
2024-08-20 18:27:52 +0200

    test works for mdps for the values

--- tests/test_result.py
+++ tests/test_result.py
@@ -21,22 +21,26 @@ def test_convert_model_checker_results_dtmc():
         assert False
 
 
-"""
 def test_convert_model_checker_results_mdp():
-    path = stormpy.examples.files.prism_mdp_firewire
+    path = stormpy.examples.files.prism_mdp_coin_2_2
+
     prism_program = stormpy.parse_prism_program(path)
-    #formula_str = "P=? [F s=7 & d=2]"
-    #properties = stormpy.parse_properties(formula_str, prism_program)
+    formula_str = 'Pmin=? [F "finished" & "all_coins_equal_1"]'
+    properties = stormpy.parse_properties(formula_str, prism_program)
 
-    #model = stormpy.build_model(prism_program, properties)
-    model = stormpy.build_model(prism_program)
-    #result = stormpy.model_checking(model, properties[0])
+    model = stormpy.build_model(prism_program, properties)
+    result = stormpy.model_checking(model, properties[0], extract_scheduler=True)
 
-    print(model)
+    # print(dir(result.scheduler))
+
+    # print(result.scheduler.get_choice(0).get_choice())
 
     stormvogel_model = stormvogel.map.stormpy_to_stormvogel(model)
-    #if stormvogel_model is not None:
-    #    stormvogel.result.convert_model_checking_result(stormvogel_model, result)
-
-    print(stormvogel_model)
-"""
+    if stormvogel_model is not None:
+        stormvogel_result = stormvogel.result.convert_model_checking_result(
+            stormvogel_model, result
+        )
+        assert list(stormvogel_result.values.values()) == result.get_values()
+        # assert [result.scheduler.get_choice(state.id).get_choice() for state in model.states] == list(stormvogel_result.scheduler.taken_actions.values())
+    else:
+        assert False
