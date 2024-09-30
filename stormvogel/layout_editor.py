649d5781de1776c5137c288e502c3c7da3591cb6
YouGuessedMyName <ivo@melse.info>
2024-09-30 18:49:05 +0200

    Start naive value iteration

--- stormvogel/layout_editor.py
+++ stormvogel/layout_editor.py
@@ -58,7 +58,7 @@ Please contact the stormvogel developpers if you keep running into issues."""
                 logging.debug(f"Status of vis {self.vis}")
             if self.vis is not None:
                 with self.output:
-                    if not stormvogel.communication_server.internal_enable_server:
+                    if stormvogel.communication_server.server is None:
                         with self.debug_output:
                             logging.info(
                                 "Did not save node positions because the server is disabled."
