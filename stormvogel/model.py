f4e02532b1cff365468aad0e04312a1067c06989
YouGuessedMyName <ivo@melse.info>
2024-11-23 17:07:04 +0100

    fix test

--- stormvogel/model.py
+++ stormvogel/model.py
@@ -213,7 +213,7 @@ class Action:
 
     def __eq__(self, other):
         if isinstance(other, Action):
-            return self.name == other.name and self.labels == other.labels
+            return self.labels == other.labels
         return False
 
 
