024cfeeb6ef4b040b621a8d6ad58201a86395086
PimLeerkes <pimsimon.leerkes@gmail.com>
2025-02-11 12:55:10 +0100

    pgc now also works with integers instead of pgc state objects

--- tests/test_pgc.py
+++ tests/test_pgc.py
@@ -55,9 +55,92 @@ def test_pgc_mdp():
 
     # we build the model in the regular way:
     regular_model = model.new_mdp(create_initial_state=False)
-    state1 = regular_model.new_state(labels=["init", "1"], features={"x": 1})
-    state2 = regular_model.new_state(labels=["2"], features={"x": 2})
-    state0 = regular_model.new_state(labels=["0"], features={"x": 0})
+    state1 = regular_model.new_state(labels=["init", "1"])
+    state2 = regular_model.new_state(labels=["2"])
+    state0 = regular_model.new_state(labels=["0"])
+    left = regular_model.new_action(frozenset({"left"}))
+    right = regular_model.new_action(frozenset({"right"}))
+    branch12 = model.Branch([(0.5, state1), (0.5, state2)])
+    branch10 = model.Branch([(0.5, state1), (0.5, state0)])
+    branch01 = model.Branch([(0.5, state0), (0.5, state1)])
+    branch21 = model.Branch([(0.5, state2), (0.5, state1)])
+
+    regular_model.add_transitions(
+        state1, model.Transition({left: branch12, right: branch10})
+    )
+    regular_model.add_transitions(state2, model.Transition({right: branch21}))
+    regular_model.add_transitions(state0, model.Transition({left: branch01}))
+
+    rewardmodel = regular_model.add_rewards("rewardmodel: 0")
+    for i in range(2 * N):
+        pair = regular_model.get_state_action_pair(i)
+        assert pair is not None
+        rewardmodel.set_state_action_reward(pair[0], pair[1], 1)
+    rewardmodel = regular_model.add_rewards("rewardmodel: 1")
+    for i in range(2 * N):
+        pair = regular_model.get_state_action_pair(i)
+        assert pair is not None
+        rewardmodel.set_state_action_reward(pair[0], pair[1], 2)
+
+    assert regular_model == pgc_model
+
+
+def test_pgc_mdp_int():
+    # we build the model with pgc:
+    N = 2
+    p = 0.5
+    initial_state = math.floor(N / 2)
+
+    left = pgc.Action(["left"])
+    right = pgc.Action(["right"])
+
+    def available_actions(s):
+        if s == 1:
+            return [left, right]
+        elif s == 2:
+            return [right]
+        else:
+            return [left]
+
+    def rewards(s, a: pgc.Action):
+        return [1, 2]
+
+    def labels(s):
+        return [str(s)]
+
+    def delta(s, action: pgc.Action):
+        if action == left:
+            return (
+                [
+                    (p, s + 1),
+                    (1 - p, s),
+                ]
+                if s < N
+                else []
+            )
+        elif action == right:
+            return (
+                [
+                    (p, s - 1),
+                    (1 - p, s),
+                ]
+                if s > 0
+                else []
+            )
+
+    pgc_model = pgc.build_pgc(
+        delta=delta,
+        available_actions=available_actions,
+        initial_state_pgc=initial_state,
+        labels=labels,
+        rewards=rewards,
+    )
+
+    # we build the model in the regular way:
+    regular_model = model.new_mdp(create_initial_state=False)
+    state1 = regular_model.new_state(labels=["init", "1"])
+    state2 = regular_model.new_state(labels=["2"])
+    state0 = regular_model.new_state(labels=["0"])
     left = regular_model.new_action(frozenset({"left"}))
     right = regular_model.new_action(frozenset({"right"}))
     branch12 = model.Branch([(0.5, state1), (0.5, state2)])
