41b662085ff1ce412cd2a759ca0bcbdb0d57c019
PimLeerkes <pimsimon.leerkes@gmail.com>
2025-02-27 15:26:47 +0100

    rational functions als polynomials can now be printed

--- stormvogel/parametric_representation.py
+++ stormvogel/parametric_representation.py
@@ -1,19 +1,6 @@
 import numpy as np
 
 
-class Parameter:
-    name: str
-
-    def __init__(self, name: str):
-        self.name = name
-
-    def __str__(self) -> str:
-        return self.name
-
-
-# Parameter = str
-
-
 class Polynomial:
     coefficients: np.ndarray = np.array([])
 
@@ -25,9 +12,18 @@ class Polynomial:
 
     def __str__(self) -> str:
         s = ""
-        for id in np.ndindex(self.coefficients.shape):
-            s += f"Index: {id}, Value: {self.coefficients[id]}"
-        return s
+        for index, id in enumerate(np.ndindex(self.coefficients.shape)):
+            if self.coefficients[id] != 0:
+                if self.coefficients[id] != 1:
+                    s += f"{self.coefficients[id]}*"
+                for i in range(len(id)):
+                    if id[i] != 0:
+                        s += f"x_{i}"
+                        if id[i] != 1:
+                            s += f"^{id[i]}"
+                s += " + "
+
+        return s[:-3]
 
 
 class RationalFunction:
@@ -47,13 +43,12 @@ class RationalFunction:
 
 
 if __name__ == "__main__":
-    x = Parameter("x")
-    y = Parameter("y")
     polynomial1 = Polynomial(3, 3)
     polynomial2 = Polynomial(4, 4)
+    polynomial1.set_coefficient((0, 0, 1), 5)
+    polynomial1.set_coefficient((3, 2, 1), 2.4)
+    polynomial2.set_coefficient((2, 3, 2, 1), 1)
 
-    print(polynomial1)
+    rationalfunction = RationalFunction(polynomial1, polynomial2)
 
-    # function = RationalFunction(polynomial1, polynomial2)
-
-    # print(function)
+    print(rationalfunction)
