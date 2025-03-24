0447c6df88daaf24fd028795260118843d5ac137
YouGuessedMyName <ivo@melse.info>
2025-03-24 16:25:59 +0100

    debugging notebooks progress

---
+++ stormvogel/examples/end_components.py
@@ -0,0 +1,36 @@
+from stormvogel import pgc
+from stormvogel.model import ModelType
+
+
+def create_end_components_mdp():
+    init = pgc.State(x=["init"])
+
+    def available_actions(s: pgc.State):
+        if "init" in s.x:
+            return [pgc.Action(["one"]), pgc.Action(["two"])]
+        elif "mec1" in s.x or "mec2" in s.x:
+            return [pgc.Action(["one"]), pgc.Action(["two"])]
+        return [pgc.Action([])]
+
+    def delta(s: pgc.State, a: pgc.Action):
+        if "init" in s.x and "one" in a.labels:
+            return [(0.5, pgc.State(x=["mec1"])), (0.5, pgc.State(x=["mec2"]))]
+        elif "mec1" in s.x:
+            return [(1, pgc.State(x=["mec2"]))]
+        elif "mec2" in s.x:
+            return [(1, pgc.State(x=["mec1"]))]
+        elif "init" in s.x and "two" in a.labels:
+            return [(1, pgc.State(x=["mec1"]))]
+        return [(1, s)]
+
+    def labels(s):
+        return s.x
+
+    mdp = pgc.build_pgc(
+        delta=delta,
+        initial_state_pgc=init,
+        available_actions=available_actions,
+        labels=labels,
+        modeltype=ModelType.MDP,
+    )
+    return mdp
