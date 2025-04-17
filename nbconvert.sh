95a0d83a23f2f741fd0fa4677c64a14e076a69e1
YouGuessedMyName <ivo@melse.info>
2025-04-17 09:27:23 +0200

    renamed notebooks to old_notebooks and removed some old stuff

--- nbconvert.sh
+++ nbconvert.sh
@@ -1,1 +1,2 @@
 poetry run jupyter nbconvert --execute --to notebook --inplace "docs/**/**.ipynb"  --ExecutePreprocessor.kernel_name=python3
+# Useful for testing nbconvert (part of the pipeline) locally
