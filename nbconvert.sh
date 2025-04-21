0fd664f979da73d0c3ca0980950cef2d453fdd69
YouGuessedMyName <ivo@melse.info>
2025-04-21 10:25:09 +0200

    remove two scripts from root

--- nbconvert.sh
+++
@@ -1,2 +0,0 @@
-poetry run jupyter nbconvert --execute --to notebook --inplace "docs/**/**.ipynb"  --ExecutePreprocessor.kernel_name=python3
-# Useful for testing nbconvert (part of the pipeline) locally
--- start.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-source env/bin/activate
-poetry shell
-# Shorthand script to open two virtual environments at once
