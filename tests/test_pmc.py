10251e7cce37772ede17070faecc17a168638591
PimLeerkes <pimsimon.leerkes@gmail.com>
2025-04-24 16:54:18 +0200

    small changes

--- tests/test_pmc.py
+++ tests/test_pmc.py
@@ -1,8 +1,12 @@
 import stormvogel.examples.pmc
+import stormvogel.mapping as mapping
 
 
 def test_pmc():
     dtmc = stormvogel.examples.pmc.create_die_dtmc()
+
+    # TODO mapping
+    
 
     # TODO valuations
 
