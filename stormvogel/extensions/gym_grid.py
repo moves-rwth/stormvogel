acf4290cc005ba6fe7e465503690351dd25e7a20
YouGuessedMyName <ivo@melse.info>
2025-07-09 11:40:47 +0200

    fix old action syntax

--- stormvogel/extensions/gym_grid.py
+++ stormvogel/extensions/gym_grid.py
@@ -16,10 +16,10 @@ def gymnasium_grid_to_stormvogel(
     TRANSITIONS = env.unwrapped.P
     NO_ACTIONS = env.action_space.n
     INV_MAP = {v: k for k, v in action_label_map.items()}
-    ALL_ACTIONS = [pgc.Action([x]) for x in action_label_map.values()]
+    ALL_ACTIONS = [[x] for x in action_label_map.values()]
 
     def action_numer_map(a: pgc.Action):
-        return INV_MAP[a.labels[0]]
+        return INV_MAP[a[0]]
 
     if "taxi" in env.spec.id.lower():
         # For Taxi, we need a special initial state that goes to every state. This is to account for the randomized starting position.
