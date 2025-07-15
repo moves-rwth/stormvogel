8c3b7f0690dac8d8161fcd6fbfdbc97a0e61f354
YouGuessedMyName <ivo@melse.info>
2025-07-15 12:53:51 +0200

    add blend_colors function

--- stormvogel/visualization.py
+++ stormvogel/visualization.py
@@ -33,6 +33,24 @@ def random_color() -> str:
     return "#" + "".join([random.choice("0123456789ABCDEF") for j in range(6)])
 
 
+def blend_colors(c1: str, c2: str, factor: float) -> str:
+    """Blend two colors in HEX format. #RRGGBB.
+    Args:
+        color1 (str): Color 1 in HEX format #RRGGBB
+        color2 (str): Color 2 in HEX format #RRGGBB
+        factor (float): The fraction of the resulting color that should come from color1."""
+    r1 = int("0x" + c1[1:3], 0)
+    g1 = int("0x" + c1[3:5], 0)
+    b1 = int("0x" + c1[5:7], 0)
+    r2 = int("0x" + c2[1:3], 0)
+    g2 = int("0x" + c2[3:5], 0)
+    b2 = int("0x" + c2[5:7], 0)
+    r_res = int(factor * r1 + (1 - factor) * r2)
+    g_res = int(factor * g1 + (1 - factor) * g2)
+    b_res = int(factor * b1 + (1 - factor) * b2)
+    return "#" + "".join("%02x" % i for i in [r_res, g_res, b_res])
+
+
 class Visualization(stormvogel.displayable.Displayable):
     """Handles visualization of a Model using a Network from stormvogel.network."""
 
