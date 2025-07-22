f234c7e0750d46db3df2363c0785b14496b307a7
Ossmos <flashthemusic@gmx.de>
2025-07-22 19:00:51 +0200

    Add tests for graph creation on MDP and POMDP examples

---
+++ tests/test_graph.py
@@ -0,0 +1,19 @@
+import pytest
+
+import stormvogel.examples as examples
+from stormvogel.graph import ModelGraph
+
+
+@pytest.mark.parametrize(
+    "model",
+    [
+        examples.create_monty_hall_mdp(),
+        examples.create_monty_hall_pomdp(),
+        examples.create_lion_mdp(),
+    ],
+)
+def test_graph_creation(model):
+    G = ModelGraph.from_model(model)
+    assert all(state.id in G.nodes for state in model.states.values()), (
+        "Missing state in ModelGraph"
+    )
